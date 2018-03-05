a8e9e6f4bb3927b77b5412fc543ff5fbb9e9f0ea
Azarias Ruiz <azaruiz94@hotmail.com>
2018-03-05 13:35:09 -0300

    mensaje

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Rol.create(id: "1", nombre: "admin")
-Rol.create(id: "2", nombre: "Gestor Contensioso - Adm")
-Rol.create(id: "3", nombre: "Gestor Civil-Laboral")
-
-Cliente.create(nombre: "Maria Jose", direccion: "Avda. Costanera", telefono: "12322342", email: "maria@gmail.com", ruc: "232133-2", tipo_cliente_id: "1")
-Cliente.create(nombre: "Juan Perez", direccion: "Gral. Artigas No. 2334", telefono: "7565564", email: "juanperez@gmail.com", ruc: "834784-5", tipo_cliente_id: "2")
-Cliente.create(nombre: "Carlos Mateo", direccion: "Avda Caballero", telefono: "3453434", email: "carlosm@gmail.com",ruc: "9486973-3", tipo_cliente_id: "2")
-
-user= Usuario.new
-user= Usuario.create(nombre: "Abog. Dario Lopez", email: "dario@gmail.com", empleado_id: "1", rol_id: "1", password: "123456", password_confirmation: "123456")
-user= Usuario.create(nombre: "Lic. Marta Torres", email: "marta@gmail.com", empleado_id: "2", rol_id: "2", password: "123456", password_confirmation: "123456")
-user= Usuario.create(nombre: "Dr. Ruben Miranda", email: "ruben@gmail.com", empleado_id: "3", rol_id: "3", password: "123456", password_confirmation: "123456")
-
 Departamento.create(nombre: "Itapúa")
 Departamento.create(nombre: "Misiones")
 Departamento.create(nombre: "Central")
@@ -26,6 +13,19 @@ Departamento.create(nombre: "Central")
 Ciudad.create(nombre: "Encarnación", departamento_id: "1")
 Ciudad.create(nombre: "Cambyreta", departamento_id: "1")
 Ciudad.create(nombre: "Cnel. Bogado", departamento_id: "1")
+
+Rol.create(id: "1", nombre: "admin")
+Rol.create(id: "2", nombre: "Gestor Contensioso - Adm")
+Rol.create(id: "3", nombre: "Gestor Civil-Laboral")
+
+Empleado.create(nombre: "Abog. Dario Lopez", direccion: "Corrientes N° 234", telefono: "833453423", email: "dario@gmail.com", ruc: "343537-4", ciudad_id: "1")
+Empleado.create(nombre: "Lic. Marta Torres", direccion: "Constitucion Nacional N° 68", telefono: "445345534", email: "marta@gmail.com", ruc: "363434-4", ciudad_id: "2")
+Empleado.create(nombre: "Dr. Ruben Miranda", direccion: "Avda. Irrazabal", telefono: "9375344", email: "ruben@gmail.com", ruc: "698394-4", ciudad_id: "3")
+
+user= Usuario.new
+user= Usuario.create(nombre: "Abog. Dario Lopez", email: "dario@gmail.com", empleado_id: "1", rol_id: "1", password: "123456", password_confirmation: "123456")
+user= Usuario.create(nombre: "Lic. Marta Torres", email: "marta@gmail.com", empleado_id: "2", rol_id: "2", password: "123456", password_confirmation: "123456")
+user= Usuario.create(nombre: "Dr. Ruben Miranda", email: "ruben@gmail.com", empleado_id: "3", rol_id: "3", password: "123456", password_confirmation: "123456")
 
 TipoCliente.create(nombre: "Fisico")
 TipoCliente.create(nombre: "Jurídico")
@@ -40,13 +40,9 @@ Objeto.create(nombre: "un objeto laboral especial", proceso: "Laboral", tipo_pro
 Objeto.create(nombre: "un objeto laboral ordinario", proceso: "Laboral", tipo_proceso: "Ordinario")
 Objeto.create(nombre: "un objeto laboral sumario", proceso: "Laboral", tipo_proceso: "Sumario")
 
-
-
-Empleado.create(nombre: "Abog. Dario Lopez", direccion: "Corrientes N° 234", telefono: "833453423", email: "dario@gmail.com", ruc: "343537-4", ciudad_id: "1")
-Empleado.create(nombre: "Lic. Marta Torres", direccion: "Constitucion Nacional N° 68", telefono: "445345534", email: "marta@gmail.com", ruc: "363434-4", ciudad_id: "2")
-Empleado.create(nombre: "Dr. Ruben Miranda", direccion: "Avda. Irrazabal", telefono: "9375344", email: "ruben@gmail.com", ruc: "698394-4", ciudad_id: "3")
-
-Institucion.create(nombre: "Entidad Binacional Yacyreta", direccion: "Circuito Comercial - Encarnacion", telefonoA: "021222345", email: "eby@gov.com.py", ruc: "32756556-5", departamento_id: "1", autoridad: "Ing. Poncho López")
+Cliente.create(nombre: "Maria Jose", direccion: "Avda. Costanera", telefono: "12322342", email: "maria@gmail.com", ruc: "232133-2", tipo_cliente_id: "1")
+Cliente.create(nombre: "Juan Perez", direccion: "Gral. Artigas No. 2334", telefono: "7565564", email: "juanperez@gmail.com", ruc: "834784-5", tipo_cliente_id: "2")
+Cliente.create(nombre: "Carlos Mateo", direccion: "Avda Caballero", telefono: "3453434", email: "carlosm@gmail.com",ruc: "9486973-3", tipo_cliente_id: "2")
 
 Accion.create(nombre: "Ver Procesos")
 Accion.create(nombre: "Crear Procesos")
@@ -92,3 +88,5 @@ Accion.create(nombre: "Ver Depatamentos")
 Accion.create(nombre: "Crear Depatamentos")
 Accion.create(nombre: "Editar Depatamentos")
 Accion.create(nombre: "Eliminar Depatamentos")
+
+Institucion.create(nombre: "Entidad Binacional Yacyreta", direccion: "Circuito Comercial - Encarnacion", telefonoA: "021222345", email: "eby@gov.com.py", ruc: "32756556-5", departamento_id: "1", autoridad: "Ing. Poncho López")
